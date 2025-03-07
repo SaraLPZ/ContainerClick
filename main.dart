ea9b4ea801ea2c70dbfb8f3458f92752612d4b47
SaraLPZ <79020122+SaraLPZ@users.noreply.github.com>
2025-03-07 16:08:22 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+InkWell(
+                      child: Container(
+                        width: 100,
+                        height: 150,
+                        color: Colors.amber,
+                        margin: EdgeInsets.all(20),
+                      ),
+                      onTap: () {
+                        print("Container clicked");
+                      },
+                    ),
